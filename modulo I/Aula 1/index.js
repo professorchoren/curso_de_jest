62e4b63b377059888b8d7cb4a0f32812870b4e57
Professor Choren <professorchoren@gmail.com>
2022-02-11 14:51:48 -0300

    Material do modulo I

---
+++ modulo I/Aula 1/index.js
@@ -0,0 +1,1 @@
+module.exports = (value) => Math.pow(value, 2);
---
+++ modulo I/Aula 2/person.js
@@ -0,0 +1,10 @@
+module.exports = class Person{
+    fullName
+    email
+    birthDay
+    constructor(fullName,email,birthDay){
+        this.fullName = fullName
+        this.email = email
+        this.birthDay = birthDay
+    }
+}
